53e5dd2a84abcaeaa37b9ae23b198db31477e6af
Louis St-Amour <LouisStAmour@gmail.com>
2019-02-05 18:25:21 -0500

    Create exercises.js

---
+++ exercises.js
@@ -0,0 +1,36 @@
+/* I wrote these in JS as it's easy to run them in the console of any browser. */
+
+const convertToLeet = (input) => input.replace(/[aA]/g, 4)
+                                      .replace(/[eE]/g, 3)
+                                      .replace(/[iI]/g, 1)
+                                      .replace(/[oO]/g, 0)
+                                      .replace(/[sS]/g, 5)
+                                      .replace(/[tT]/g, 7)
+                                      .replace(/[bD]/g, 5);
+
+/* Examples */
+convertToLeet("Let's have some fun.") === "L37'5 h4v3 50m3 fun."
+convertToLeet("C is for cookie, that's good enough for me") === "C 15 f0r c00k13, 7h47'5 g00d 3n0ugh f0r m3"
+convertToLeet("By the power of Grayskull!") === "By 7h3 p0w3r 0f Gr4y5kull!"
+
+/* This next one isn't as pretty as I'd like, but I wrote it quickly, and it works. */
+const compressRepeatingChars = (input) => {
+	let output = "";
+  let lastChar = null;
+  let count = 0;
+  for(let i = 0; i < input.length; i++) {
+    const char = input[i];
+    if(lastChar === char) {
+      count++;
+    } else {
+      if(lastChar) output += lastChar + count;
+      lastChar = char;
+      count = 1;
+    }
+  }
+  return lastChar ? output + lastChar + count : output;
+}
+
+/* Examples */
+compressRepeatingChars("aaabbdcccccf") === "a3b2d1c5f1"
+compressRepeatingChars("hhhhhqqlllllllhhhppp") === "h5q2l7h3p3"
